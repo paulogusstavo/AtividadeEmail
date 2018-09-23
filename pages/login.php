4b9cc5a2e25f313f514f563aa2995d02dda53b26
Paulo Gustavo Zanese <paulo_gz@hotmail.com>
2018-09-23 01:17:38 -0300

    Tela de Login
    Criada Tela de Login inicial - Resolve #1

---
+++ pages/login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+  			<meta charset="UTF-8">
+  			<title>Email - Login</title>
+  
+			<link rel="stylesheet" href="../css/login.css">
+		
+		</head>
+	<body>	
+		<div class="titulo">
+  			<h1>PAULO'S EMAIL</h1>
+  			<span>Atividade Semestral - Programação WEB</span>
+		</div>
+
+		<div class="dadosAcesso">
+  		<div class="form">
+    		<h2>Acesso ao sistema</h2>
+    		<form action="Login.php" method="POST">
+
+      			<input type="text" placeholder="Usuário" name="user" id="user" />
+      			<input type="password" placeholder="Senha" name="pass" id="pass" />
+      	
+      			<button>Entrar</button>
+    		</form>
+  		</div>
+</div>
+
+	</body>
+</html>
+
+		
+	
+		
